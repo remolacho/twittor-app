1e672695cf2d8881789f7e9daad823078dd4f859
Jonathan Rojas <jonathan.rojas@rankmi.com>
2023-05-29 13:33:37 -0500

    added image logo

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, {useState} from "react"
 import SignInSignUp from "./pages/SignInSignUp"
 
 function App() {
-    const [user, setUser] = useState(    {user: {
-            name: "Jonathan"
-        }
-    })
+    const [user, setUser] = useState(    null);
 
     return (
         <div>
             {user ? (
-                <SignInSignUp />
+                <h1>No logueado...</h1>
+
             ) : (
-               <h1>No logueado...</h1>
+                <SignInSignUp />
             )}
         </div>
     )
--- src/pages/SignInSignUp/SignInSignUp.js
+++ src/pages/SignInSignUp/SignInSignUp.js
@@ -1,5 +1,8 @@
 import React from "react";
 import {Container, Row, Col, Button } from "react-bootstrap";
+import LogoWhite from "../../assets/png/logo-white.png";
+import LogoBlue from "../../assets/png/logo.png";
+import "./SignInSignUp.scss";
 
 function SignInSignUp(){
     return (
@@ -14,15 +17,27 @@ function SignInSignUp(){
 
 function SideBarLeft() {
     return (
-        <Col className="signIn-signUp__left">
-            <h2>Sidebar Left...</h2>
+        <Col className="signIn-signUp__left" xa={6}>
+            <img src={LogoBlue} alt="twittor" />
+
+            <div>
+                <h2>
+                    - Text 1 ...
+                </h2>
+                <h2>
+                    - text 2 ...
+                </h2>
+                <h2>
+                    - text 3 ...
+                </h2>
+            </div>
         </Col>
     )
 }
 
 function RightComponent() {
     return (
-        <Col className="signIn-signUp__right">
+        <Col className="signIn-signUp__right" xs={6}>
             <h2>ComponentRight...</h2>
         </Col>
     )
